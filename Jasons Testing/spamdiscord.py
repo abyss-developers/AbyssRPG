c670d56ab4fa856b3e339400a90a7a8bc23e273a
jatgm <jasonzhou055@gmail.com>
2020-04-26 21:35:45 -0700

    holy hecking crappy

--- Jasons Testing/spamdiscord.py
+++ Jasons Testing/spamdiscord.py
@@ -4,3 +4,9 @@ script = "I WROTE A PYTHON SCRIPT THAT SPAMS PEOPLE DO YOU HAVE ANYONE IN MIND"
 for x in script.split():
     pyautogui.write(x)
     pyautogui.press("enter")
+
+#pyautogui.press("up")
+#pyautogui.hotkey("ctrl", "a")
+#pyautogui.press("delete")
+#pyautogui.press("enter")
+#pyautogui.press("enter")
