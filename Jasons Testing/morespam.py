3903ec0e46145203f5911fbb2e533c5b6c106520
jatgm <jasonzhou055@gmail.com>
2020-04-30 11:24:51 -0700

    dshfdhdhfshashdf

--- Jasons Testing/morespam.py
+++ Jasons Testing/morespam.py
@@ -1,10 +1,11 @@
 import pyautogui
 
-pyautogui.write("**Hello**")
-pyautogui.hotkey("shift", "enter")
-pyautogui.write("> This is Jason's attempt at automating talking so he doesn't have to talk to people")
-pyautogui.hotkey("shift", "enter")
-pyautogui.write("> This message was completely automated and Jason didn't have to send anything")
-pyautogui.hotkey("shift", "enter")
-pyautogui.write("> And also please @ me for things you need by the real jason!!")
-pyautogui.press("enter")
+while True:
+    pyautogui.write("**Hello**")
+    pyautogui.hotkey("shift", "enter")
+    pyautogui.write("> This is Jason's attempt at automating talking so he doesn't have to talk to people")
+    pyautogui.hotkey("shift", "enter")
+    pyautogui.write("> This message was completely automated and Jason didn't have to send anything")
+    pyautogui.hotkey("shift", "enter")
+    pyautogui.write("> And also please @ me for things you need by the real jason!!")
+    pyautogui.press("enter", interval=1)
