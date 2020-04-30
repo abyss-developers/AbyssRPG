f3a3050e3560eb160c6c02b6ae054e02298920f8
jatgm <jasonzhou055@gmail.com>
2020-04-30 16:22:10 -0700

    jhef

--- Jasons Testing/Annoy AbyssDev/morespam.py
+++ Jasons Testing/Annoy AbyssDev/morespam.py
@@ -1,12 +1,18 @@
 import pyautogui
-import pygame
+
+e = 0
 
 while True:
+    if e >= 20:
+        pyautogui.write("The machine has exceeded 20 reminders. I shall now break.")
+        break
     pyautogui.write("**Water Rehydration Reminder**")
     pyautogui.hotkey("shift", "enter")
-    pyautogui.write("> This message is automatically sent by Jason's code to remind you that you need to stay hydrated.")
+    pyautogui.write("This message is automatically sent by Jason's code to remind you that you need to stay hydrated.")
     pyautogui.hotkey("shift", "enter")
-    pyautogui.write("> This message will sent in 10 more minutes.")
+    pyautogui.write("This message will sent in 5 more minutes.")
     pyautogui.hotkey("shift", "enter")
-    pyautogui.write("> Have a nice day!")
-    pyautogui.press("enter", interval=600)
+    pyautogui.write("Have a nice day!", interval=20)
+    pyautogui.press("enter")
+    e += 1    
+    
