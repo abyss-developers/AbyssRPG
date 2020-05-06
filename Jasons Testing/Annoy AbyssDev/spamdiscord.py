948fa5153be15839e518f09cba496c3c0b215507
jatgm <jasonzhou055@gmail.com>
2020-05-06 09:10:40 -0700

    oh yeah

--- Jasons Testing/Annoy AbyssDev/spamdiscord.py
+++ Jasons Testing/Annoy AbyssDev/spamdiscord.py
@@ -2,9 +2,7 @@ import pyautogui
 script = "e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e e"
 
 for x in script.split():
-    pyautogui.hotkey("ctrl", "t")
-    pyautogui.write("https://www.youtube.com/watch?v=mPVDGOVjRQ0")
-    pyautogui.press("enter")
+    pyautogui.press("enter", interval=1 )
 
 #pyautogui.press("up")
 #pyautogui.hotkey("ctrl", "a")
