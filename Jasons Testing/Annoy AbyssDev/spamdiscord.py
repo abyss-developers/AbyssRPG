d046c28cba161c4e940f3020b2b8bf5b128997c9
jatgm <jasonzhou055@gmail.com>
2020-05-13 19:27:27 -0700

    Moved some files around

--- Jasons Testing/Annoy AbyssDev/spamdiscord.py
+++ Jasons Testing/Annoy AbyssDev/spamdiscord.py
@@ -1,9 +1,14 @@
 import pyautogui
 script = "hanging off my earlobe is a rock"
 
-for x in script.split():
-    pyautogui.write(x)
-    pyautogui.press("enter", interval=1 )
+#for x in script.split():
+#    pyautogui.write(x)
+#    pyautogui.press("enter", interval=1 )
+
+for i in range(1000):
+    pyautogui.write("bruhffefefefefeffefefefefef")
+    for e in range(27):
+        pyautogui.press("backspace")
 
 #pyautogui.press("up")
 #pyautogui.hotkey("ctrl", "a")
